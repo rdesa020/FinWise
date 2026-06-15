6d8b4e016b5eeb93c3222e8340d1842a91f68525
Riti Desai <ritidesai03@gmail.com>
2026-06-15 16:45:38 +0530

    use dynamic PORT env var for deployment

--- server.js
+++ server.js
@@ -313,5 +313,5 @@ app.post('/signin', async (req, res) => {
   }
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
